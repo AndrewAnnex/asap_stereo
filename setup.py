957b1a0ad6911df84eb3c06aaa7b7a3236dcce43
AndrewAnnex <ama6fy@virginia.edu>
2018-04-06 11:33:04 -0400

    updated classifiers to remove py2.7

--- setup.py
+++ setup.py
@@ -58,11 +58,7 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Scientific/Engineering :: GIS'
